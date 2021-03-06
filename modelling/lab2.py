56a3ec49970dba8774839c35fc546b48aab1906e
readysloth <readysloth@protonmail.com>
2021-03-06 16:42:11 +0300

    feat(lab2.py): Сделал первое задание

--- modelling/lab2.py
+++ modelling/lab2.py
@@ -18,14 +18,29 @@ def first_task():
                 yield x_next
         return rand_generator()
 
+    def calculate_period_and_show(seq):
+        period = len(list(it.takewhile(lambda e: e != seq[0],
+                                       seq[1:]))) + 1
+        print(seq[:20])
+        print('Период генератора равен ', period)
+        sns.set_style('darkgrid')
+        sns.displot(seq, kde=True)
+        plt.show()
+
+    # Хорошие значения
     rand_generator = create_random(a=6364136223846793005,
-                                   x0=99,
+                                   x0=990,
                                    c=1442695040888963407,
                                    m=2**64)
+    #rand_generator = create_random(a=10,
+    #                               x0=90,
+    #                               c=10,
+    #                               m=111)
+    random_seq = list(it.islice(rand_generator, 100000))
+    random_sub_seq = [e % 10 for e in random_seq]
+    calculate_period_and_show(random_seq)
+    calculate_period_and_show(random_sub_seq)
 
-    sns.set_style('darkgrid')
-    sns.displot(list(it.islice(rand_generator, 100000)),
-                kde=True)
-    plt.show()
+
 
 first_task()
