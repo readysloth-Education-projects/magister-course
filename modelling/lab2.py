2844ed56b4e19c84add24bb2b854db4883f3be5c
readysloth <readysloth@protonmail.com>
2021-03-12 21:16:44 +0300

    feat(lab2.py): Завершил лабораторную

--- modelling/lab2.py
+++ modelling/lab2.py
@@ -4,6 +4,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
+def calculate_period_and_show(seq):
+    period = len(list(it.takewhile(lambda e: e != seq[0],
+                                   seq[1:]))) + 1
+    print(seq[:20])
+    print('Период генератора равен ', period)
+    sns.set_style('darkgrid')
+    sns.displot(seq, kde=True)
+    plt.show()
+
+
 def first_task():
     def create_random(a,x0,c,m):
         assert m  >= 2
@@ -17,15 +27,6 @@ def first_task():
                 x_next = (a*x_next + c) % m
                 yield x_next
         return rand_generator()
-
-    def calculate_period_and_show(seq):
-        period = len(list(it.takewhile(lambda e: e != seq[0],
-                                       seq[1:]))) + 1
-        print(seq[:20])
-        print('Период генератора равен ', period)
-        sns.set_style('darkgrid')
-        sns.displot(seq, kde=True)
-        plt.show()
 
     # Хорошие значения
     rand_generator = create_random(a=6364136223846793005,
@@ -42,5 +43,18 @@ def first_task():
     calculate_period_and_show(random_sub_seq)
 
 
+def second_task():
+    import random
+    import math
+    def rand_generator():
+        while True:
+            fi = random.random()
+            r = random.random()
+            yield math.cos(2*math.pi*fi)*math.sqrt(-2*math.log(r))
+            yield math.sin(2*math.pi*fi)*math.sqrt(-2*math.log(r))
+
+    random_seq = list(it.islice(rand_generator(), 100000))
+    calculate_period_and_show(random_seq)
 
 first_task()
+second_task()
