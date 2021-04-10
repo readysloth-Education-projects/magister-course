f5bc61fc374d319984fc40c451526c5f4939beb4
readysloth <readysloth@protonmail.com>
2021-04-10 08:24:45 +0300

    refact(opencl/lab2.py): Вынес общее тело в отдельную функцию

--- opencl/lab2.py
+++ opencl/lab2.py
@@ -4,54 +4,56 @@ import numpy as np
 from PIL import Image
 from icecream import ic
 
-
-def make_grayscale():
+def process_image(cl_text, function_name, output_img_name):
     image = Image.open('image.jpg')
     data = np.asarray(image)
-
     ctx, queue = clw.get_context_and_queue()
-    prog = clw.build_program(ctx, """
-                            #define max_of_three(m,n,p) ( (m) > (n) ? ((m) > (p) ? (m) : (p)) : ((n) > (p) ? (n) : (p)))
-                            #define min_of_three(m,n,p) ( (m) < (n) ? ((m) < (p) ? (m) : (p)) : ((n) < (p) ? (n) : (p)))
+    prog = clw.build_program(ctx, cl_text)
 
-                            __kernel void make_grayscale(__global char *rgb_row,
-                                                         __global char *gray_row)
-                            {
-                              int gid = get_global_id(0);
-                              int rgb_chunk_start = gid * 3;
-                              char max_light = max_of_three(rgb_row[rgb_chunk_start],
-                                                           rgb_row[rgb_chunk_start+1],
-                                                           rgb_row[rgb_chunk_start+2]);
-                              char min_light = min_of_three(rgb_row[rgb_chunk_start],
-                                                           rgb_row[rgb_chunk_start+1],
-                                                           rgb_row[rgb_chunk_start+2]);
-                              char gray_color = (min_light + max_light) / 2;
-                              gray_row[rgb_chunk_start]   = gray_color;
-                              gray_row[rgb_chunk_start+1] = gray_color;
-                              gray_row[rgb_chunk_start+2] = gray_color;
-                            }
-                            """)
-
-    make_grayscale = prog.make_grayscale
+    cl_func = getattr(prog, function_name)
 
     def process_rows(func, rows):
 
         flatten_rows = rows.flatten()
         row_cl = clw.bind_to_buffer(ctx, flatten_rows)
-        gray_row, time = clw.execute_kernel(func, ctx, queue, flatten_rows, row_cl)
+        p_row, time = clw.execute_kernel(func, ctx, queue, flatten_rows, row_cl)
 
-        return np.array(np.array_split(gray_row, len(gray_row) // 3)), time;
+        return np.array(np.array_split(p_row, len(p_row) // 3)), time;
 
     time = 0
     columns = []
     for d in data:
-        grayed, time1 = process_rows(make_grayscale, d)
+        processed, time1 = process_rows(cl_func, d)
         time += time1
-        columns.append(grayed)
+        columns.append(processed)
 
     new_data = np.array(columns)
-    Image.fromarray(new_data).save("grayscale.jpg")
+    Image.fromarray(new_data).save(output_img_name)
 
     return time
+
+def make_grayscale():
+
+    process_image( """
+    #define max_of_three(m,n,p) ( (m) > (n) ? ((m) > (p) ? (m) : (p)) : ((n) > (p) ? (n) : (p)))
+    #define min_of_three(m,n,p) ( (m) < (n) ? ((m) < (p) ? (m) : (p)) : ((n) < (p) ? (n) : (p)))
+
+    __kernel void make_grayscale(__global char *rgb_row,
+                                 __global char *gray_row)
+    {
+      int gid = get_global_id(0);
+      int rgb_chunk_start = gid * 3;
+      char max_light = max_of_three(rgb_row[rgb_chunk_start],
+                                   rgb_row[rgb_chunk_start+1],
+                                   rgb_row[rgb_chunk_start+2]);
+      char min_light = min_of_three(rgb_row[rgb_chunk_start],
+                                   rgb_row[rgb_chunk_start+1],
+                                   rgb_row[rgb_chunk_start+2]);
+      char gray_color = (min_light + max_light) / 2;
+      gray_row[rgb_chunk_start]   = gray_color;
+      gray_row[rgb_chunk_start+1] = gray_color;
+      gray_row[rgb_chunk_start+2] = gray_color;
+    }""", "make_grayscale", "grayscale.jpg")
+
 
 make_grayscale()
