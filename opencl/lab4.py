3d27f7f4218d52cb6a1add4c6169f715aaa1c4a2
readysloth <readysloth@protonmail.com>
2021-04-25 15:16:08 +0300

    feat(opencl/lab4.py): Завершил лабу (на моем устройстве не работает)

--- opencl/lab4.py
+++ opencl/lab4.py
@@ -44,10 +44,6 @@ box_blur_program = {
            BLUR(RED)
            BLUR(GREEN)
            BLUR(BLUE)
-
-           FOREACH_PIXEL_IN_CHUNK(pixel_index, GET_COLOR_INDEX(RED, blur_row, pixel_index)   = CREATE_SUM_NAME(RED))
-           FOREACH_PIXEL_IN_CHUNK(pixel_index, GET_COLOR_INDEX(GREEN, blur_row, pixel_index) = CREATE_SUM_NAME(GREEN))
-           FOREACH_PIXEL_IN_CHUNK(pixel_index, GET_COLOR_INDEX(BLUE, blur_row, pixel_index)  = CREATE_SUM_NAME(BLUE))
            """,
 
   "signature" : """
@@ -98,6 +94,10 @@ def process_image_via_buffers(image_name):
                         {box_blur_program['signature']}
                         {{
                           {box_blur_program['body']}
+
+                          FOREACH_PIXEL_IN_CHUNK(pixel_index, GET_COLOR_INDEX(RED, blur_row, pixel_index)   = CREATE_SUM_NAME(RED))
+                          FOREACH_PIXEL_IN_CHUNK(pixel_index, GET_COLOR_INDEX(GREEN, blur_row, pixel_index) = CREATE_SUM_NAME(GREEN))
+                          FOREACH_PIXEL_IN_CHUNK(pixel_index, GET_COLOR_INDEX(BLUE, blur_row, pixel_index)  = CREATE_SUM_NAME(BLUE))
                         }}
                         """
     blurred_data, box_blur_time = process_buffer(box_blur_text, data, box_blur_program['kernel'], np.int32(10))
@@ -118,8 +118,8 @@ def process_image_via_pipes(image_name):
     image = Image.open(image_name)
     data = np.asarray(image)
     blur_and_grey_text = f"""
-                        #define SPIN_READ_PIPE(PIPE, DEST)  while(read_pipe(PIPE, &DEST));
-                        #define SPIN_WRITE_PIPE(PIPE, DEST) while(write_pipe(PIPE, &DEST));
+                        #define SPIN_READ_PIPE(PIPE, DEST)  if(read_pipe(PIPE, &DEST)){{mem_fence(CLK_GLOBAL_MEM_FENCE);}};
+                        #define SPIN_WRITE_PIPE(PIPE, DEST) if(write_pipe(PIPE, &DEST)){{return;}};
 
                         {box_blur_program['defines']}
                         __kernel void box_blur(int rgb_row_len,
@@ -130,17 +130,15 @@ def process_image_via_pipes(image_name):
                                                __global char *blur_row)
                         {{
                           {box_blur_program['body']}
-                          FOREACH_PIXEL_IN_CHUNK(pixel_index,
-                                                 SPIN_WRITE_PIPE(gray_pipe, GET_COLOR_INDEX(RED, blur_row, pixel_index));
-                                                 SPIN_WRITE_PIPE(gray_pipe, GET_COLOR_INDEX(GREEN, blur_row, pixel_index));
-                                                 SPIN_WRITE_PIPE(gray_pipe, GET_COLOR_INDEX(BLUE, blur_row, pixel_index));)
 
+                          char red = CREATE_SUM_NAME(RED);
+                          char green = CREATE_SUM_NAME(GREEN);
+                          char blue = CREATE_SUM_NAME(BLUE);
+                          FOREACH_PIXEL_IN_CHUNK(pixel_index,
+                                                 SPIN_WRITE_PIPE(gray_pipe, red);
+                                                 SPIN_WRITE_PIPE(gray_pipe, green);
+                                                 SPIN_WRITE_PIPE(gray_pipe, blue);)
                           mem_fence(CLK_GLOBAL_MEM_FENCE);
-
-                          FOREACH_PIXEL_IN_CHUNK(pixel_index,
-                                                 SPIN_READ_PIPE(blur_pipe, GET_COLOR_INDEX(RED, blur_row, pixel_index));
-                                                 SPIN_READ_PIPE(blur_pipe, GET_COLOR_INDEX(GREEN, blur_row, pixel_index));
-                                                 SPIN_READ_PIPE(blur_pipe, GET_COLOR_INDEX(BLUE, blur_row, pixel_index));)
                         }}
 
                         {grayscale_program['defines']}
@@ -165,23 +163,46 @@ def process_image_via_pipes(image_name):
                            SPIN_WRITE_PIPE(blur_pipe, gray_color);
                            SPIN_WRITE_PIPE(blur_pipe, gray_color);
                         }}
+
+
+                        __kernel void collector(int rgb_row_len,
+                                                __global char *rgb_row,
+                                                int blur_size,
+                                                write_only pipe char gray_pipe,
+                                                read_only pipe char blur_pipe,
+                                                __global char *blur_row)
+                        {{
+                          int gid = get_global_id(0);
+
+                          int chunk_start = gid * 3 * blur_size;
+                          int chunk_end = chunk_start + 3 * blur_size;
+
+                          if(chunk_start > rgb_row_len){{
+                            return;
+                          }}
+                          FOREACH_PIXEL_IN_CHUNK(pixel_index,
+                                                 SPIN_READ_PIPE(blur_pipe, GET_COLOR_INDEX(RED, blur_row, pixel_index));
+                                                 SPIN_READ_PIPE(blur_pipe, GET_COLOR_INDEX(GREEN, blur_row, pixel_index));
+                                                 SPIN_READ_PIPE(blur_pipe, GET_COLOR_INDEX(BLUE, blur_row, pixel_index));)
+                        }}
                         """
 
     ctx, _ = clw.get_context_and_queue()
-    pipe = clw.get_rw_pipe(ctx)
+    pipe = clw.get_rw_pipe(ctx, 10, 4096)
 
     blurred_and_grey_data, blurred_and_grey_time = process_buffer(blur_and_grey_text,
                                                                   data,
-                                                                  [box_blur_program['kernel'],
-                                                                   grayscale_program['kernel']],
+                                                                  [box_blur_program["kernel"],
+                                                                   grayscale_program["kernel"],
+                                                                   "collector"],
                                                                   np.int32(10),
                                                                   pipe,
                                                                   pipe,
-                                                                  multiple=True)
-
+                                                                  multiple=True,
+                                                                  options=['-cl-std=CL2.0'])
     Image.fromarray(blurred_and_grey_data).save("blurred_and_gray.jpg")
 
     return blurred_and_grey_time
 
-#ic(process_image_via_buffers("image.jpg"))
-ic(process_image_via_pipes("image.jpg"))
+ic(process_image_via_buffers("image_512x512.jpg"))
+ic(process_image_via_pipes("image_512x512.jpg"))
