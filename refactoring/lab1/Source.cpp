b75b73eda4644eae27e5916af42e414068e66e3e
readysloth <readysloth@protonmail.com>
2021-11-27 22:52:30 +0300

    refact(refactoring/lab1/Source.cpp): Добавили освобождение памяти, перешли на c++17, часть поведния выделено в функции

--- refactoring/lab1/Source.cpp
+++ refactoring/lab1/Source.cpp
@@ -10,6 +10,26 @@
 typedef std::vector<int> row_t;
 typedef std::vector<row_t> matrix_t;
 
+void* MEMORY_POOL[512] = {nullptr};
+int MEMORY_POOL_HEAD = 0;
+
+
+#define NEW(TYPE, WRITE_VAL) auto ptr ## TYPE ## WRITE_VAL = new TYPE();\
+														 MEMORY_POOL[MEMORY_POOL_HEAD] = ptr ## TYPE ## WRITE_VAL; \
+														 MEMORY_POOL_HEAD++; \
+														 WRITE_VAL = *(ptr ## TYPE ## WRITE_VAL);
+
+#define FREE() for(int i = 0; i < MEMORY_POOL_HEAD; i ++) {delete MEMORY_POOL[i];}
+
+
+#define INIT_MATRIX(word, num, size) \
+	matrix_t word ## num; \
+	NEW(matrix_t, word ## num);\
+	word ## num.resize(size);\
+	    for (auto &row : word ## num){\
+			row.resize(size);\
+    	}
+
 template<typename IT, int Count>
 std::vector<IT> get_user_input(const std::string &out_text,
 															 const std::string &failure_text,
@@ -17,7 +37,7 @@ std::vector<IT> get_user_input(const std::string &out_text,
 	std::vector<IT> input_array;
 
 	std::cout << out_text << std::endl;
-	for(int i = 0; i < Count; i++){
+	for(int x_dim= 0; x_dim< Count; x_dim++){
 		while(true){
 			IT elem;
 			std::cin >> elem;
@@ -47,7 +67,7 @@ std::tuple<int, int> get_dimensions(const std::string &out_text){
 void populate_row(row_t &row,
 									int count,
 									std::function<int()> &populate_function){
-	for(int i = 0; i < count; i++){
+	for(int x_dim= 0; x_dim< count; x_dim++){
 		row.push_back(populate_function());
 	}
 }
@@ -59,9 +79,10 @@ void populate_matrix(matrix_t &matrix,
 	int x_dim = std::get<0>(dimensions);
 	int y_dim = std::get<1>(dimensions);
 	for(int i = 0; i < x_dim; i++){
-		auto row = new row_t();
-		matrix.push_back(*row);
-		populate_row(*row, y_dim, populate_function);
+		row_t row;
+		NEW(row_t, row);
+		populate_row(row, y_dim, populate_function);
+		matrix.push_back(row);
 	}
 }
 
@@ -72,8 +93,28 @@ void print_matrix(matrix_t &matrix){
 		}
 		std::cout << std::endl;
 	}
+	std::cout << std::endl;
 }
 
+
+auto get_tuple_of_partitioned_matrix(matrix_t &default_mat, int dimension)
+{
+	INIT_MATRIX(blockMatrix, 11, dimension/2);
+	INIT_MATRIX(blockMatrix, 12, dimension/2);
+	INIT_MATRIX(blockMatrix, 21, dimension/2);
+	INIT_MATRIX(blockMatrix, 22, dimension/2);
+    for (int x_dim = 0; x_dim < dimension/2; x_dim++)
+    {
+        for (int y_dim = 0; y_dim < dimension/2; y_dim++)
+        {
+			blockMatrix11[x_dim][y_dim] = default_mat[x_dim][y_dim];
+			blockMatrix12[x_dim][y_dim] = default_mat[x_dim][y_dim + dimension / 2];
+			blockMatrix21[x_dim][y_dim] = default_mat[x_dim+ dimension / 2][y_dim];
+			blockMatrix22[x_dim][y_dim] = default_mat[x_dim+ dimension / 2][y_dim+ dimension / 2];
+        }
+    }
+	return std::make_tuple(blockMatrix11, blockMatrix12, blockMatrix21, blockMatrix22);
+}
 
 int main()
 {
@@ -83,10 +124,9 @@ int main()
 	auto first_matrix_dimensions = get_dimensions("Введите размеры первой матрицы");
 	auto second_matrix_dimensions = get_dimensions("Введите размеры второй матрицы");
 
-	auto first_matrix_ptr = new matrix_t();
-	auto second_matrix_ptr = new matrix_t();
-	auto first_matrix = *first_matrix_ptr;
-	auto second_matrix = *second_matrix_ptr;
+	matrix_t first_matrix, second_matrix;
+	NEW(matrix_t, first_matrix);
+	NEW(matrix_t, second_matrix);
 
 
 	std::function<bool(int)> failure_choice = [](int choice){return !(1 == choice || choice == 2);};
@@ -146,319 +186,90 @@ int main()
 	///////////////Разбиение матриц на подматрицы и их заполнение//////////////////
 	///////////////////////////////////////////////////////////////////////////////
 
-	//int** mat1 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat1[i] = new int[l / 2];
-	//	for (int j = 0; j < l / 2; j++)
-	//		mat1[i][j] = M3[i][j];
-	//}
-	//int** mat2 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat2[i] = new int[l / 2];
-	//	for (int j = 0; j < l / 2; j++)
-	//		mat2[i][j] = M3[i][j + l / 2];
-	//}
-	//int** mat3 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat3[i] = new int[l / 2];
-	//	for (int j = 0; j < l / 2; j++)
-	//		mat3[i][j] = M3[i + l / 2][j];
-	//}
-	//int** mat4 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat4[i] = new int[l / 2];
-	//	for (int j = 0; j < l / 2; j++)
-	//		mat4[i][j] = M3[i + l / 2][j + l / 2];
-	//}
-	//int** mat5 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat5[i] = new int[l / 2];
-	//	for (int j = 0; j < l / 2; j++)
-	//		mat5[i][j] = M4[i][j];
-	//}
-	//int** mat6 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat6[i] = new int[l / 2];
-	//	for (int j = 0; j < l / 2; j++)
-	//		mat6[i][j] = M4[i][j + l / 2];
-	//}
-	//int** mat7 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat7[i] = new int[l / 2];
-	//	for (int j = 0; j < l / 2; j++)
-	//		mat7[i][j] = M4[i + l / 2][j];
-	//}
-	//int** mat8 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat8[i] = new int[l / 2];
-	//	for (int j = 0; j < l / 2; j++)
-	//		mat8[i][j] = M4[i + l / 2][j + l / 2];
-	//}
+	auto first_part_matrix_tuple = get_tuple_of_partitioned_matrix(first_matrix, extended_size);
+	auto second_part_matrix_tuple = get_tuple_of_partitioned_matrix(second_matrix, extended_size);
+	std::cout << "Блочные матрицы 1:" << std::endl;
+    std::apply([](auto&&... matrix) {(print_matrix(matrix), ...);}, first_part_matrix_tuple);
+	std::cout << "Блочные матрицы 2:" << std::endl;
+    std::apply([](auto&&... matrix) {(print_matrix(matrix), ...);}, second_part_matrix_tuple);
+
 
 	/////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////Создание промежуточных матриц//////////////////////////
 	/////////////////////////////////////////////////////////////////////////////////
 
-	//int** p1 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	p1[i] = new int[l / 2];
-	//}
-	//int** p2 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	p2[i] = new int[l / 2];
-	//}
-	//int** p3 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	p3[i] = new int[l / 2];
-	//}
-	//int** p4 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	p4[i] = new int[l / 2];
-	//}
-	//int** p5 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	p5[i] = new int[l / 2];
-	//}
-	//int** p6 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	p6[i] = new int[l / 2];
-	//}
-	//int** p7 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	p7[i] = new int[l / 2];
-	//}
-
+	INIT_MATRIX(p, 1, extended_size/2);
+	INIT_MATRIX(p, 2, extended_size/2);
+	INIT_MATRIX(p, 3, extended_size/2);
+	INIT_MATRIX(p, 4, extended_size/2);
+	INIT_MATRIX(p, 5, extended_size/2);
+	INIT_MATRIX(p, 6, extended_size/2);
+	INIT_MATRIX(p, 7, extended_size/2);
+	
 	/////////////////////////////////////////////////////////////////////////////////
 	//////////////////////Вычисление значений промежуточных матриц///////////////////
 	/////////////////////////////////////////////////////////////////////////////////
 
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	for (int j = 0; j < l / 2; j++)
-	//	{
-	//		p1[i][j] = 0;
-	//		for (int z = 0; z < l / 2; z++)
-	//		{
-	//			p1[i][j] += (mat1[i][z] + mat4[i][z]) * (mat5[z][j] + mat8[z][j]);
-	//		}
-
-	//		p2[i][j] = 0;
-	//		for (int z = 0; z < l / 2; z++)
-	//		{
-	//			p2[i][j] += (mat3[i][z] + mat4[i][z]) * mat5[z][j];
-	//		}
-
-	//		p3[i][j] = 0;
-	//		for (int z = 0; z < l / 2; z++)
-	//		{
-	//			p3[i][j] += mat1[i][z] * (mat6[z][j] - mat8[z][j]);
-	//		}
-
-	//		p4[i][j] = 0;
-	//		for (int z = 0; z < l / 2; z++)
-	//		{
-	//			p4[i][j] += mat4[i][z] * (mat7[z][j] - mat5[z][j]);
-	//		}
-
-	//		p5[i][j] = 0;
-	//		for (int z = 0; z < l / 2; z++)
-	//		{
-	//			p5[i][j] += (mat1[i][z] + mat2[i][z]) * mat8[z][j];
-	//		}
-
-	//		p6[i][j] = 0;
-	//		for (int z = 0; z < l / 2; z++)
-	//		{
-	//			p6[i][j] += (mat3[i][z] - mat1[i][z]) * (mat5[z][j] + mat6[z][j]);
-	//		}
-
-	//		p7[i][j] = 0;
-	//		for (int z = 0; z < l / 2; z++)
-	//		{
-	//			p7[i][j] += (mat2[i][z] - mat4[i][z]) * (mat7[z][j] + mat8[z][j]);
-	//		}
-	//	}
-	//}
-
-	/////////////////////////////////////////////////////////////////////////////////
-	/////////////////////////Создание вспомогательных матриц/////////////////////////
-	/////////////////////////////////////////////////////////////////////////////////
-
-	//int** mat9 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat9[i] = new int[l / 2];
-	//}
-	//int** mat10 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat10[i] = new int[l / 2];
-	//}
-	//int** mat11 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat11[i] = new int[l / 2];
-	//}
-	//int** mat12 = new int* [l / 2];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	mat12[i] = new int[l / 2];
-	//}
-
-	/////////////////////////////////////////////////////////////////////////////////
-	//////////////Подсчет значений вспомогательных матриц из промежуточных///////////
-	/////////////////////////////////////////////////////////////////////////////////
-
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	for (int j = 0; j < l / 2; j++)
-	//	{
-	//		mat9[i][j] = p1[i][j] + p4[i][j] - p5[i][j] + p7[i][j];
-	//		mat10[i][j] = p3[i][j] + p5[i][j];
-	//		mat11[i][j] = p2[i][j] + p4[i][j];
-	//		mat12[i][j] = p1[i][j] - p2[i][j] + p3[i][j] + p6[i][j];
-	//	}
-	//}
-
-	/////////////////////////////////////////////////////////////////////////////////
-	/////////////////////Создание результирующей матрицы/////////////////////////////
-	/////////////////////////////////////////////////////////////////////////////////
-
-	//int** M5 = new int* [l];
-	//for (int i = 0; i < l; i++)
-	//{
-	//	M5[i] = new int[l];
-	//}
-
-	/////////////////////////////////////////////////////////////////////////////////
-	/////////Занесение информации из вспомогательных матриц в результирующую/////////
-	/////////////////////////////////////////////////////////////////////////////////
-
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	for (int j = 0; j < l / 2; j++)
-	//	{
-	//		M5[i][j] = mat9[i][j];
-	//		M5[i][j + l / 2] = mat10[i][j];
-	//		M5[i + l / 2][j] = mat11[i][j];
-	//		M5[i + l / 2][j + l / 2] = mat12[i][j];
-	//	}
-	//}
-
+	for (int x_dim= 0; x_dim< extended_size / 2; x_dim++)
+	{
+		for (int y_dim = 0; y_dim < extended_size / 2; y_dim++)
+		{
+			for (int z = 0; z < extended_size / 2; z++)
+			{
+				p1[x_dim][y_dim] += (std::get<0>(first_part_matrix_tuple)[x_dim][z] + std::get<3>(first_part_matrix_tuple)[x_dim][z]) * (std::get<0>(second_part_matrix_tuple)[z][y_dim] + std::get<3>(second_part_matrix_tuple)[z][y_dim]);
+				p2[x_dim][y_dim] += (std::get<2>(first_part_matrix_tuple)[x_dim][z] + std::get<3>(first_part_matrix_tuple)[x_dim][z]) * std::get<0>(second_part_matrix_tuple)[z][y_dim];
+				p3[x_dim][y_dim] += std::get<0>(first_part_matrix_tuple)[x_dim][z] * (std::get<1>(second_part_matrix_tuple)[z][y_dim] - std::get<3>(second_part_matrix_tuple)[z][y_dim]);
+				p4[x_dim][y_dim] += std::get<3>(first_part_matrix_tuple)[x_dim][z] * (std::get<2>(second_part_matrix_tuple)[z][y_dim] - std::get<0>(second_part_matrix_tuple)[z][y_dim]);
+				p5[x_dim][y_dim] += (std::get<0>(first_part_matrix_tuple)[x_dim][z] + std::get<1>(first_part_matrix_tuple)[x_dim][z]) * std::get<3>(second_part_matrix_tuple)[z][y_dim];
+				p6[x_dim][y_dim] += (std::get<2>(first_part_matrix_tuple)[x_dim][z] - std::get<0>(first_part_matrix_tuple)[x_dim][z]) * (std::get<0>(second_part_matrix_tuple)[z][y_dim] + std::get<1>(second_part_matrix_tuple)[z][y_dim]);
+				p7[x_dim][y_dim] += (std::get<1>(first_part_matrix_tuple)[x_dim][z] - std::get<3>(first_part_matrix_tuple)[x_dim][z]) * (std::get<2>(second_part_matrix_tuple)[z][y_dim] + std::get<3>(second_part_matrix_tuple)[z][y_dim]);
+			}
+		}
+	}
+    INIT_MATRIX(result, _matrix, extended_size);
+	for (int x_dim= 0; x_dim< extended_size / 2; x_dim++)
+	{
+		for (int y_dim = 0; y_dim < extended_size / 2; y_dim++)
+		{
+			result_matrix[x_dim][y_dim] = p1[x_dim][y_dim] + p4[x_dim][y_dim] - p5[x_dim][y_dim] + p7[x_dim][y_dim];
+			result_matrix[x_dim][y_dim + extended_size / 2] = p3[x_dim][y_dim] + p5[x_dim][y_dim];
+			result_matrix[x_dim+ extended_size / 2][y_dim] = p2[x_dim][y_dim] + p4[x_dim][y_dim];
+			result_matrix[x_dim+ extended_size / 2][y_dim + extended_size / 2] = p1[x_dim][y_dim] - p2[x_dim][y_dim] + p3[x_dim][y_dim] + p6[x_dim][y_dim];
+		}
+	}
 	/////////////////////////////////////////////////////////////////////////////////
 	//////////////////Выравнивание границ результирующей матрицы/////////////////////
 	/////////////////////////////////////////////////////////////////////////////////
+	int x = 0, y=0, sizeResMatrixX = extended_size, sizeResMatrixY = extended_size;
+	for (int x_dim= 0; x_dim< extended_size; x_dim++)
+	{
+		x = 0;
+		y = 0;
+		for (int y_dim = 0; y_dim < extended_size; y_dim++)
+		{
+			if (result_matrix[x_dim][y_dim] != 0)
+			{
+				x++;
+				sizeResMatrixX = extended_size;
+			}
+			if (result_matrix[y_dim][x_dim] != 0)
+			{
+				y++;
+				sizeResMatrixY = extended_size;
+			}
+		}
+		if (x == 0 && x_dim< sizeResMatrixX)
+		{
+			sizeResMatrixX = x_dim;
+		}
+		if (y == 0 && x_dim< sizeResMatrixY)
+		{
+			sizeResMatrixY = x_dim;
+		}
+	}
 
-	//int x = 0, f = 100, s = 100;
-	//for (int i = 0; i < l; i++)
-	//{
-	//	x = 0;
-	//	for (int j = 0; j < l; j++)
-	//	{
-	//		if (M5[i][j] != 0)
-	//		{
-	//			x++;
-	//			f = 100;
-	//		}
-	//	}
-	//	if (x == 0 && i < f)
-	//	{
-	//		f = i;
-	//	}
-	//}
-	//for (int i = 0; i < l; i++)
-	//{
-	//	x = 0;
-	//	for (int j = 0; j < l; j++)
-	//	{
-	//		if (M5[j][i] != 0)
-	//		{
-	//			x++;
-	//			s = 100;
-	//		}
-	//	}
-	//	if (x == 0 && i < s)
-	//	{
-	//		s = i;
-	//	}
-	//}
+	std::cout << "Результирующая матрица" << std::endl;
+	print_matrix(result_matrix);
+	FREE();
 
-	//int** M6 = new int* [f];
-	//for (int i = 0; i < f; i++)
-	//{
-	//	M6[i] = new int[s];
-	//	for (int j = 0; j < s; j++)
-	//		M6[i][j] = M5[i][j];
-	//}
-
-	/////////////////////////////////////////////////////////////////////////////////
-	/////////////////////Вывод результирующей матрицы////////////////////////////////
-	/////////////////////////////////////////////////////////////////////////////////
-
-	//std::cout << "\nРезультирующая матрица\n\n";
-	//for (int i = 0; i < f; i++)
-	//{
-	//	for (int j = 0; j < s; j++)
-	//		std::cout << M6[i][j] << " ";
-	//	std::cout << endl;
-	//}
-
-	//system("pause");
-
-	/////////////////////////////////////////////////////////////////////////////////
-	///////////////////////Очистка динамической памяти///////////////////////////////
-	/////////////////////////////////////////////////////////////////////////////////
-	//
-	//for (int i = 0; i < n1; i++)
-	//	delete[] M1[i];
-	//for (int i = 0; i < n2; i++)
-	//	delete[] M2[i];
-	//for (int i = 0; i < l; i++)
-	//{
-	//	delete[] M3[i];
-	//	delete[] M4[i];
-	//	delete[] M5[i];
-	//}
-	//for (int i = 0; i < f; i++)
-	//	delete[] M6[i];
-	//for (int i = 0; i < l / 2; i++)
-	//{
-	//	delete[] mat1[i];
-	//	delete[] mat2[i];
-	//	delete[] mat3[i];
-	//	delete[] mat4[i];
-	//	delete[] mat5[i];
-	//	delete[] mat6[i];
-	//	delete[] mat7[i];
-	//	delete[] mat8[i];
-	//	delete[] mat9[i];
-	//	delete[] mat10[i];
-	//	delete[] mat11[i];
-	//	delete[] mat12[i];
-	//	delete[] p1[i];
-	//	delete[] p2[i];
-	//	delete[] p3[i];
-	//	delete[] p4[i];
-	//	delete[] p5[i];
-	//	delete[] p6[i];
-	//	delete[] p7[i];
-	//}
-	//delete[] M1, M2, M3, M4, M5, M6;
-	//delete[] mat1, mat2, mat3, mat4, mat5, mat6, mat7, mat8, mat9, mat10, mat11, mat12;
-	//delete[] p1, p2, p3, p4, p5, p6, p7;
 }
